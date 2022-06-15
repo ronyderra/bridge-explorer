1188b84d3091d7a64078a307aa9c99204a027ca9
ronyXnes <derrarony@gmail.com>
2022-06-15 18:02:41 +0300

    formatting

--- src/services/helpers.ts
+++ src/services/helpers.ts
@@ -3,27 +3,27 @@ import { BigNumber } from "@ethersproject/bignumber";
 import { isHexString, isBytes } from "@ethersproject/bytes";
 import { BigNumberish } from "@ethersproject/bignumber";
 
-const saveWallet = async  function (eventRepo:IEventRepo, senderAddress:string | undefined, to:string | undefined) {
+const saveWallet = async function (eventRepo: IEventRepo, senderAddress: string | undefined, to: string | undefined) {
     Promise.all([
-        senderAddress ? eventRepo.findWallet(senderAddress): undefined,
-        to? eventRepo.findWallet(to): undefined
+        senderAddress ? eventRepo.findWallet(senderAddress) : undefined,
+        to ? eventRepo.findWallet(to) : undefined
     ]).then(async ([walletFrom, walletTo]) => {
-        if (!walletFrom && senderAddress) await eventRepo.createWallet({address: senderAddress});
-        if (!walletTo && to && senderAddress !== to) await eventRepo.createWallet({address: to!});
+        if (!walletFrom && senderAddress) await eventRepo.createWallet({ address: senderAddress });
+        if (!walletTo && to && senderAddress !== to) await eventRepo.createWallet({ address: to! });
     }).catch((err) => console.log(err))
 }
 
-function isBigNumberish(value: any): value is BigNumberish {
+const isBigNumberish = (value: any): value is BigNumberish => {
     return (value != null) && (
         BigNumber.isBigNumber(value) ||
-        (typeof(value) === "number" && (value % 1) === 0) ||
-        (typeof(value) === "string" && !!value.match(/^-?[0-9]+$/)) ||
+        (typeof (value) === "number" && (value % 1) === 0) ||
+        (typeof (value) === "string" && !!value.match(/^-?[0-9]+$/)) ||
         isHexString(value) ||
-        (typeof(value) === "bigint") ||
+        (typeof (value) === "bigint") ||
         isBytes(value)
     );
 }
 
-export {saveWallet, isBigNumberish}
+export { saveWallet, isBigNumberish }
 
 
