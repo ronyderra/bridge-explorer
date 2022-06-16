fd9b5fabd71c2f55a5ebd7e5fbbc6e58bb59c3a9
ronyXnes <derrarony@gmail.com>
2022-06-16 16:17:02 +0300

    elrond formatting

--- src/listeners/Elrond/elrond.ts
+++ src/listeners/Elrond/elrond.ts
@@ -6,7 +6,7 @@ import { IERC721WrappedMeta } from "../../Intrerfaces/ERCMeta";
 import { IEvent } from "../../Intrerfaces/IEvent";
 import { io } from "socket.io-client";
 import { clientAppSocket } from "../../index";
-import {Address,ProxyProvider,} from "@elrondnetwork/erdjs";
+import { Address, ProxyProvider, } from "@elrondnetwork/erdjs";
 import { IDatabaseDriver, Connection, EntityManager } from "@mikro-orm/core";
 import createEventRepo from "../../business-logic/repo";
 import { eventFromTxn, bigIntFromBeElrd, getFrozenTokenAttrs } from "./helper";
@@ -25,109 +25,108 @@ const minterAddr = new Address(config.elrond.contract);
 
 
 // TODO: Save bridge events to db
-export  function elrondEventListener(
-  em: EntityManager<IDatabaseDriver<Connection>>,
+export function elrondEventListener(
+    em: EntityManager<IDatabaseDriver<Connection>>,
 ): IContractEventListener {
 
-  return {
-    listen: async () => {
-      elrondSocket.on("elrond:bridge_tx", async (fromHash: string) => {
-        try {
-          console.log(fromHash, "fromHash");
-          const event = await eventFromTxn(fromHash, provider, providerRest);
+    return {
+        listen: async () => {
+            elrondSocket.on("elrond:bridge_tx", async (fromHash: string) => {
+                try {
+                    console.log(fromHash, "fromHash");
+                    const event = await eventFromTxn(fromHash, provider, providerRest);
 
-          if (!event) return;
+                    if (!event) return;
 
-          event.evs.length &&
-            event.evs.forEach(async (e) => {
+                    event.evs.length &&
+                        event.evs.forEach(async (e) => {
 
-              if (e.topics.length < 5) {
-                return undefined;
-              }
-              if (e.address != config.elrond.contract) {
-                return undefined;
-              }
+                            if (e.topics.length < 5) {
+                                return undefined;
+                            }
+                            if (e.address != config.elrond.contract) {
+                                return undefined;
+                            }
 
-              const action_id = bigIntFromBeElrd(
-                Base64.toUint8Array(e.topics[1])
-              );
-              const tx_fees = bigIntFromBeElrd(
-                Base64.toUint8Array(e.topics[e.topics.length - 1])
-              );
+                            const action_id = bigIntFromBeElrd(
+                                Base64.toUint8Array(e.topics[1])
+                            );
+                            const tx_fees = bigIntFromBeElrd(
+                                Base64.toUint8Array(e.topics[e.topics.length - 1])
+                            );
 
-              console.log({
-                action_id: action_id.toString(),
-                tx_fees: tx_fees.toString(),
-              });
+                            console.log({
+                                action_id: action_id.toString(),
+                                tx_fees: tx_fees.toString(),
+                            });
 
-              console.log(e.topics);
-              const to = Base64.atob(e.topics[3]); //
-              const nftMinterContact = Base64.decode(e.topics[4]); //
-              let uri = "";
-              let tokenId = "";
-              let chain_nonce = new Uint32Array(
-                Base64.toUint8Array(e.topics[2])
-              )[0]; //
-              const nonce = bigIntFromBeElrd(Base64.toUint8Array(e.topics[6]));
+                            console.log(e.topics);
+                            const to = Base64.atob(e.topics[3]); //
+                            const nftMinterContact = Base64.decode(e.topics[4]); //
+                            let uri = "";
+                            let tokenId = "";
+                            let chain_nonce = new Uint32Array(
+                                Base64.toUint8Array(e.topics[2])
+                            )[0]; //
+                            const nonce = bigIntFromBeElrd(Base64.toUint8Array(e.topics[6]));
 
-              console.log({
-                chain_nonce,
-                nftMinterContact,
-                nonce,
-              });
+                            console.log({
+                                chain_nonce,
+                                nftMinterContact,
+                                nonce,
+                            });
 
-              let type = "Unfreeze";
+                            let type = "Unfreeze";
 
-              switch (e.identifier) {
-                case "withdrawNft": {
-                  type = "Unfreeze";
+                            switch (e.identifier) {
+                                case "withdrawNft": {
+                                    type = "Unfreeze";
 
-                  uri = Base64.decode(e.topics[5]); //
-                  const wrappedData = await axios.get<IERC721WrappedMeta>(uri);
-                  tokenId = wrappedData?.data?.wrapped?.tokenId;
+                                    uri = Base64.decode(e.topics[5]); //
+                                    const wrappedData = await axios.get<IERC721WrappedMeta>(uri);
+                                    tokenId = wrappedData?.data?.wrapped?.tokenId;
 
-                  break;
-                }
-                case "freezeSendNft": {
-                  type = "Transfer";
-                  tokenId = Base64.decode(e.topics[5]);
+                                    break;
+                                }
+                                case "freezeSendNft": {
+                                    type = "Transfer";
+                                    tokenId = Base64.decode(e.topics[5]);
 
-                  const name = Base64.decode(e.topics[7]);
-                  uri = Base64.decode(e.topics[8]);
-                  const [attrs, metadataUrl] = await getFrozenTokenAttrs(
-                    tokenId,
-                    nonce
-                  );
+                                    const name = Base64.decode(e.topics[7]);
+                                    uri = Base64.decode(e.topics[8]);
+                                    const [attrs, metadataUrl] = await getFrozenTokenAttrs(
+                                        tokenId,
+                                        nonce
+                                    );
 
-                  console.log({
-                    name,
-                    metadataUrl,
-                    attrs,
-                  });
-                  break;
-                }
-              }
+                                    console.log({
+                                        name,
+                                        metadataUrl,
+                                        attrs,
+                                    });
+                                    break;
+                                }
+                            }
 
-              const eventObj: IEvent = {
-                actionId: action_id?.toString(),
-                chainName: "ELROND",
-                tokenId,
-                fromChain: "2",
-                toChain: chain_nonce?.toString(),
-                fromChainName: chainNonceToName("2"),
-                toChainName: chainNonceToName(nonce?.toString()) || "",
-                fromHash,
-                txFees: tx_fees?.toString(),
-                type,
-                status: "Pending",
-                toHash: "",
-                senderAddress: event.sender,
-                targetAddress: to,
-                nftUri: uri || "",
-                createdAt:  new Date()
-              };
-
-              console.log("transfer event: ", eventObj);
+                            const eventObj: IEvent = {
+                                actionId: action_id?.toString(),
+                                chainName: "ELROND",
+                                tokenId,
+                                fromChain: "2",
+                                toChain: chain_nonce?.toString(),
+                                fromChainName: chainNonceToName("2"),
+                                toChainName: chainNonceToName(nonce?.toString()) || "",
+                                fromHash,
+                                txFees: tx_fees?.toString(),
+                                type,
+                                status: "Pending",
+                                toHash: "",
+                                senderAddress: event.sender,
+                                targetAddress: to,
+                                nftUri: uri || "",
+                                createdAt: new Date()
+                            };
+                            console.log("transfer event: ", eventObj);
 
                             const [doc] = await Promise.all([
                                 (async () => {
@@ -155,26 +154,30 @@ export  function elrondEventListener(
                                 }, 1000 * 60 * 20);
                             }
                         });
+                } catch (e) {
+                    console.log(e, "elrond Error");
+                }
+            });
 
-      executedSocket.on("tx_executed_event",async (fromChain: number,toChain: number,action_id: string,hash: string) => {
-          if (!fromChain || fromChain.toString() !== config.elrond.nonce)
-            return;
+            executedSocket.on("tx_executed_event", async (fromChain: number, toChain: number, action_id: string, hash: string) => {
+                if (!fromChain || fromChain.toString() !== config.elrond.nonce)
+                    return;
 
-          console.log({toChain,fromChain,action_id,hash,},"elrond:tx_executed_event");
-          executedEventHandler(em.fork(), fromChain.toString())({toChain,fromChain,action_id,hash,})
-        }
-      );
+                console.log({ toChain, fromChain, action_id, hash, }, "elrond:tx_executed_event");
+                executedEventHandler(em.fork(), fromChain.toString())({ toChain, fromChain, action_id, hash, })
+            }
+            );
 
-      setTimeout(
-        () => console.log(elrondSocket.connected && "Listening to Elrond"),
-        1000
-      );
-    },
-  };
+            setTimeout(
+                () => console.log(elrondSocket.connected && "Listening to Elrond"),
+                1000
+            );
+        },
+    };
 }
 
 export type Erc721Attrs = {
-  trait_type: string;
-  value: string;
+    trait_type: string;
+    value: string;
 };
 
