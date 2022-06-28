39257930d28ff0722c575e570902d97931ad7077
ronyXnes <derrarony@gmail.com>
2022-06-28 20:03:37 +0300

    comment getUriFa2

--- src/listeners/Tezos/getTezosData.ts
+++ src/listeners/Tezos/getTezosData.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
-import createEventRepo from "../../business-logic/repo";
-import { BlockRepo } from "../../Intrerfaces/IBlockRepo";
-import { IDatabaseDriver, Connection, wrap, EntityManager } from "@mikro-orm/core";
-import { BigMapAbstraction, MichelsonMap, TezosToolkit } from "@taquito/taquito";
-import { bytes2Char } from "@taquito/utils";
+// import createEventRepo from "../../business-logic/repo";
+// import { BlockRepo } from "../../Intrerfaces/IBlockRepo";
+// import { IDatabaseDriver, Connection, wrap, EntityManager } from "@mikro-orm/core";
+// import { BigMapAbstraction, MichelsonMap, TezosToolkit } from "@taquito/taquito";
+// import { bytes2Char } from "@taquito/utils";
 
 export const getTezosCollectionData = async (hash: string) => {
     try {
@@ -33,20 +33,20 @@ export const getTezosCollectionData = async (hash: string) => {
         console.log(err)
     }
 }
-export async function getUriFa2(fa2Address: string, tokenId: string , rpc: string): Promise<string> {
-  const tezos = new TezosToolkit(rpc);
+// export async function getUriFa2(fa2Address: string, tokenId: string , rpc: string): Promise<string> {
+//   const tezos = new TezosToolkit(rpc);
 
-    const contract = await tezos.contract.at(fa2Address);
-    const storage = await contract.storage<{
-      token_metadata: BigMapAbstraction;
-    }>();
+//     const contract = await tezos.contract.at(fa2Address);
+//     const storage = await contract.storage<{
+//       token_metadata: BigMapAbstraction;
+//     }>();
 
-    const tokenStorage = await storage.token_metadata.get<{
-      token_info: MichelsonMap<string, string>;
-    }>(tokenId);
+//     const tokenStorage = await storage.token_metadata.get<{
+//       token_info: MichelsonMap<string, string>;
+//     }>(tokenId);
 
-    const uriFa2 = bytes2Char(tokenStorage!.token_info.get("")!);
-    console.log("tezos.ts Line 72 - Uri:", uriFa2)
+//     const uriFa2 = bytes2Char(tokenStorage!.token_info.get("")!);
+//     console.log("tezos.ts Line 72 - Uri:", uriFa2)
 
-    return uriFa2;
-  }
+//     return uriFa2;
+//   }
