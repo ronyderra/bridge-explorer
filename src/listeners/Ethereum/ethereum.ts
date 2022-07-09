0f4e2bf87a2ce8b855cd0a649bd8e7f41d188fc6
ronyXnes <derrarony@gmail.com>
2022-07-09 15:09:22 +0300

    ethereum comment

--- src/listeners/Ethereum/ethereum.ts
+++ src/listeners/Ethereum/ethereum.ts
@@ -1,16 +1,16 @@
-import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
-import axios from "axios"
+// import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
+// import axios from "axios"
 
-interface IContractEventListener {
-    listenBridge(): void;
-}
+// interface IContractEventListener {
+//     listenBridge(): void;
+// }
 
-export function EvmEventService(em: EntityManager<IDatabaseDriver<Connection>>): IContractEventListener {
-    return {
-        listenBridge: async () => {
-            setInterval(async () => {
-                const dataBCD = await axios.get(`https://api.better-call.dev/v1/contract/mainnet/KT1WKtpe58XPCqNQmPmVUq6CZkPYRms5oLvu/operations?entrypoints=freeze_fa2,withdraw_nft`)
-            }, 10000)
-        }
-    }
-}
+// export function EvmEventService(em: EntityManager<IDatabaseDriver<Connection>>): IContractEventListener {
+//     return {
+//         listenBridge: async () => {
+//             setInterval(async () => {
+//                 const dataBCD = await axios.get(`https://api.better-call.dev/v1/contract/mainnet/KT1WKtpe58XPCqNQmPmVUq6CZkPYRms5oLvu/operations?entrypoints=freeze_fa2,withdraw_nft`)
+//             }, 10000)
+//         }
+//     }
+// }
